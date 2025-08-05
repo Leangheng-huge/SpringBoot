3c9e0054d3af83fb6764609964e94322f544f957
Leangheng-huge <heng525555@gmail.com>
2025-08-05 21:47:54 +0700

    refactor(suppler) : add validation for SupplierDto

--- src/main/java/com/chiikawa/demo/DTO/supplier/SupplierDto.java
+++ src/main/java/com/chiikawa/demo/DTO/supplier/SupplierDto.java
@@ -2,12 +2,16 @@ package com.chiikawa.demo.DTO.supplier;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SupplierDto {
 
-    @NotBlank(message = "supplier name is require")
+    @NotNull(message = "supplier name is require")
+    @NotBlank(message = "supplier name must not be blank")
+    @Size(min = 5, max = 20 , message = "supplier name must be between 5 to 20 characters")
     private String name;
 
     private String address;
