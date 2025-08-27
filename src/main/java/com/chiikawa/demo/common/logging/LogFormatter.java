20fd21134abe9fcb1f9e8a58802f5dbf7e1f40da
Leangheng-huge <heng525555@gmail.com>
2025-08-27 19:54:24 +0700

    feat: create LogFormatter in logging

---
+++ src/main/java/com/chiikawa/demo/common/logging/LogFormatter.java
@@ -0,0 +1,45 @@
+package com.chiikawa.demo.common.logging;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogFormatter {
+    private static final String LOG_FORMAT = "%s:%s | target=%s, method=%s, startTime=%s, endTime=%s," +
+            " executionTime=%sms";
+
+    public String logRequest(String requestId,String target,String method,Long startTime) {
+        return String.format(LOG_FORMAT,
+                "REQUEST",
+                requestId,
+                target,
+                method,
+                startTime,
+                0,
+                0
+        );
+    }
+
+    public String logResponse(String requestId,String target,String method,Long startTime,Long endTime) {
+        return String.format(LOG_FORMAT,
+                "RESPONSE",
+                requestId,
+                target,
+                method,
+                startTime,
+                endTime,
+                endTime - startTime
+        );
+    }
+
+    public String logError(String requestId,String target,String method,Long startTime,Long endTime) {
+        return String.format(LOG_FORMAT,
+                "ERROR",
+                requestId,
+                target,
+                method,
+                startTime,
+                endTime,
+                endTime - startTime
+        );
+    }
+}
--- src/main/java/com/chiikawa/demo/common/logging/LoggingAspect.java
+++ src/main/java/com/chiikawa/demo/common/logging/LoggingAspect.java
@@ -5,24 +5,44 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Aspect
 @Component
 public class LoggingAspect {
     String LOG_FORMAT = "%s | className=%s, method=%s";
+    @Autowired
+    private LogFormatter formatter;
 
     @Around("execution(* com.chiikawa.demo.service..*(..))")
-    public Object logServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable{
+    public Object logServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
         Logger log = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        // get method name , e.g. listStocks()
         String methodName = joinPoint.getSignature().getName();
+        // get class name , e.g. StockService
         String className = joinPoint.getTarget().getClass().getSimpleName();
+        long startTime = System.currentTimeMillis();
+        String requestId = UUID.randomUUID().toString();
 
-        log.info(LOG_FORMAT.formatted("Request", className,methodName));
+        log.info(formatter.logRequest(requestId,className,methodName,startTime));
 
         try {
-            return joinPoint.proceed();
-        }catch (Exception e){
+            // execute the original method logic
+            Object result = joinPoint.proceed();
+
+            long endTime = System.currentTimeMillis();
+            // logging
+            log.info(formatter.logResponse(requestId,className,methodName,startTime,endTime));
+
+            return result;
+        } catch (Exception e) {
+            long endTime = System.currentTimeMillis();
+
+            log.error(formatter.logError(requestId,className,methodName,startTime,endTime));
+
             throw e;
         }
     }
