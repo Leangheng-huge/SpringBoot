c81128a27bb74f1d33d641de7e7f407ce0ae6695
Leangheng-huge <heng525555@gmail.com>
2025-09-07 22:58:29 +0700

    refactor(SecurityConfig): add jwtAuthenticationFilter implement

--- src/main/java/com/chiikawa/demo/common/security/SecurityConfig.java
+++ src/main/java/com/chiikawa/demo/common/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.chiikawa.demo.common.security;
 
+import com.chiikawa.demo.common.filter.JwtAuthenticationFilter;
 import com.chiikawa.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +15,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,7 +35,9 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
 
-        authBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder());
+        authBuilder
+                .userDetailsService(userService)
+                .passwordEncoder(this.passwordEncoder());
 
         return authBuilder.build();
     }
@@ -39,15 +46,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authz ->
                                 authz
 //                                    .requestMatchers("/api/v1/users/**").hasRole("ADMIN")
-                                        .requestMatchers("/api/**").permitAll()
+                                        .requestMatchers("/api/v1/auth/**").permitAll()
                                         .anyRequest()
                                         .authenticated()
-                );
+                )
+                .authenticationManager(this.authenticationManager(http))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
