0f3be1c9bcbbdd71f8800be16257a40f7d06aea0
Leangheng-huge <heng525555@gmail.com>
2025-09-10 21:02:33 +0700

    feat: adding refreshToken in aAuthService

--- src/main/java/com/chiikawa/demo/service/security/AuthService.java
+++ src/main/java/com/chiikawa/demo/service/security/AuthService.java
@@ -5,6 +5,7 @@ import com.chiikawa.demo.DTO.auth.AuthDto;
 import com.chiikawa.demo.DTO.auth.AuthResponseDto;
 import com.chiikawa.demo.Exception.model.DuplicateResourceException;
 import com.chiikawa.demo.Mapper.UserMapper;
+import com.chiikawa.demo.entity.RefreshToken;
 import com.chiikawa.demo.entity.User;
 import com.chiikawa.demo.repository.UserRepository;
 import com.chiikawa.demo.service.UserService;
@@ -35,6 +36,9 @@ public class AuthService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
     public AuthResponseDto register(UserDto payload) {
         // validate if username is existed
         if(userRepository.existsByName(payload.getName())) {
@@ -52,7 +56,10 @@ public class AuthService {
         User createdUser = userRepository.save(user);
         String accessToken = jwtUtil.generateToken(createdUser);
 
-        return new AuthResponseDto(accessToken,null);
+        // generate refresh token
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(createdUser);
+
+        return new AuthResponseDto(accessToken,refreshToken.getToken());
     }
 
     public AuthResponseDto login(AuthDto payload) {
@@ -63,6 +70,10 @@ public class AuthService {
         UserDetails userDetails = userService.loadUserByUsername(payload.getUsername());
         String accessToken = jwtUtil.generateToken(userDetails);
 
-        return new AuthResponseDto(accessToken,null);
+        // generate refresh token
+        User user = (User) userDetails;
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(user);
+
+        return new AuthResponseDto(accessToken,refreshToken.getToken());
     }
 }
