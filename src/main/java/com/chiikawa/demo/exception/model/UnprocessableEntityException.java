650ae3dd1f796bcc8efa8ffbcb7e24ef86e46e57
Leangheng-huge <heng525555@gmail.com>
2025-09-21 15:04:52 +0700

    refactor: bcuz of changed the name of package exception

--- src/main/java/com/chiikawa/demo/exception/model/UnprocessableEntityException.java
+++ src/main/java/com/chiikawa/demo/exception/model/UnprocessableEntityException.java
@@ -1,4 +1,4 @@
-package com.chiikawa.demo.Exception.model;
+package com.chiikawa.demo.exception.model;
 
 public class UnprocessableEntityException extends RuntimeException{
     public UnprocessableEntityException(String message){
