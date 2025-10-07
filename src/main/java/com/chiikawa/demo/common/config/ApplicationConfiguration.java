67051bd40026f44b18d2511678efa16832bd6753
Leangheng-huge <heng525555@gmail.com>
2025-10-07 19:56:27 +0700

    feat: configuration for pagination

--- src/main/java/com/chiikawa/demo/common/config/ApplicationConfiguration.java
+++ src/main/java/com/chiikawa/demo/common/config/ApplicationConfiguration.java
@@ -4,11 +4,14 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+
 @Data
 @ConfigurationProperties(prefix = "config")
 @Configuration
 public class ApplicationConfiguration {
     private Security security;
+    private Pagination pagination;
 
    @Data
     public static class Security {
@@ -16,6 +19,16 @@ public class ApplicationConfiguration {
         private long expirationTime ;
         private long refreshTokenExpiration ;
 
+    }
+
+    @Data
+    public static class Pagination {
+       private String baseUrl;
+      private HashMap<String, String> uri;
+
+      public String getUrlByResource(String resource) {
+          return baseUrl.concat(uri.getOrDefault(resource,""));
+      }
     }
 
 }
