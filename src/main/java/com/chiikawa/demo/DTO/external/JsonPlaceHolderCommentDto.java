370ba0902b04b12ff45c69e331ab5e1108a30d95
Leangheng-huge <heng525555@gmail.com>
2025-10-22 23:11:09 +0700

    refactor: json place holder

---
+++ src/main/java/com/chiikawa/demo/DTO/external/JsonPlaceHolderCommentDto.java
@@ -0,0 +1,12 @@
+package com.chiikawa.demo.DTO.external;
+
+import lombok.Data;
+
+@Data
+public class JsonPlaceHolderCommentDto {
+    private int postId;
+    private int id;
+    private String name;
+    private String email;
+    private String body;
+}
