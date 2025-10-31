9682166eb04724d6826ae61b8e39ca7b3947de9f
Leangheng-huge <heng525555@gmail.com>
2025-10-31 16:16:56 +0700

    feat: kafka implement

---
+++ src/main/java/com/chiikawa/demo/service/kafka/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.chiikawa.demo.service.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        String topic = "testing-queue";
+
+        kafkaTemplate.send(topic, message);
+
+        log.info("message produced[{}]: {}", topic, message);
+    }
+}
